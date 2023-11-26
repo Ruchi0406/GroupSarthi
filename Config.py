00f8b1f8161a32098afcfa29cf338955abae149c
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-26 16:28:17 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', "c408c6b40e1ebd04b76c7d04a8de1dad")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "5658314356:AAEIWcIyXBsQhRrltN_Nki6Ra5GcrTY522s")
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    START_IMG = os.environ.get('START_IMG', None)
+    START_IMG = os.environ.get('START_IMG', https://graph.org/file/30603556ff2ab7b941819.jpg)
     BOT_USERNAME = os.environ.get('BOT_USERNAME', None)
     OWNER_ID=5935608297
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
@@ -24,7 +24,7 @@ else:
     API_ID = ""
     API_HASH = ""
     BOT_TOKEN = ""
-    START_IMG= ""
+    START_IMG= "https://graph.org/file/30603556ff2ab7b941819.jpg"
     DATABASE_URL = ""
     BOT_USERNAME=""
     OWNER_ID=5935608297
