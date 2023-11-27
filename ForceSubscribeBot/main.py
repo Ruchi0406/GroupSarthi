2b6f962582d181b6c8419b78d715919aa228582c
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-27 13:22:58 +0530

    Update main.py

--- ForceSubscribeBot/main.py
+++ ForceSubscribeBot/main.py
@@ -6,67 +6,63 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import ChatPermissions
+
 @Client.on_message(filters.group, group=-1)
 async def main(bot: Client, msg: Message):
     if not msg.from_user:
         return
+
     user_id = msg.from_user.id
     if user_id in Config.DEVS:
         return
+
     chat_id = msg.chat.id
     force_chat = await get_force_chat(chat_id)
     ignore_service = await get_ignore_service(chat_id)
+
     if ignore_service and msg.service:
         return
+
     if not force_chat:
         return
+
     try:
         try:
             await bot.get_chat_member(force_chat, user_id)
         except UserNotParticipant:
             chat_member = await msg.chat.get_member(user_id)
+
             if chat_member.status in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER):
                 return
+
             chat = await bot.get_chat(force_chat)
             mention = '@' + chat.username if chat.username else f"[{chat.title}]({chat.invite_link})"
             link = chat.invite_link
+
             try:
                 action = await get_action(chat_id)
-                if action == 'kick':
-                    await bot.ban_chat_member(chat_id, user_id)
-                    await bot.unban_chat_member(chat_id, user_id)
-                    await msg.reply("ᴋɪᴄᴋᴇᴅ ᴍᴇᴍʙᴇʀ ʙᴇᴄᴀᴜsᴇ  ɴᴏᴛ  ᴊᴏɪɴᴇᴅ ғᴏʀᴄᴇ sᴜʙsʀɪʙᴇ ᴄʜᴀᴛ")  
-                    await msg.delete()
-                    return
-                elif action=="nothing":
-                    return 
-                
-                elif action == 'warn':
-                    await msg.reply(f"ʜᴇʏ ᴅᴇᴀʀ  {msg.from_user.mention},\n\nʏᴏᴜ ᴍᴜsᴛ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ {mention} ᴛᴏ ᴄʜᴀᴛ ʜᴇʀᴇ ᴛʜᴇɴ ᴘʀᴇss ᴏɴ ᴜɴᴍᴜᴛᴇ ʙᴜᴛᴛᴏɴ.",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✨ sᴜʙsᴄʀɪʙᴇ  ᴍʏ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]))
-                    await msg.delete()
-                    return
 
-                elif action == 'ban':
+                # Set the default action to 'warn' if no specific action is configured
+                action = action if action else 'warn'
+
+                if action == 'warn':
+                    await msg.reply(
+                        f"ʜᴇʏ ᴅᴇᴀʀ  {msg.from_user.mention},\n\nʏᴏᴜ ᴍᴜsᴛ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ {mention} ᴛᴏ ᴄʜᴀᴛ ʜᴇʀᴇ ᴛʜᴇɴ ᴘʀᴇss ᴏɴ ᴜɴᴍᴜᴛᴇ ʙᴜᴛᴛᴏɴ.",
+                        disable_web_page_preview=True,
+                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✨ sᴜʙsᴄʀɪʙᴇ  ᴍʏ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]])
+                    )
                     await msg.delete()
-                    await bot.ban_chat_member(chat_id, user_id, datetime.now() + timedelta(days=1))
-                    await msg.reply("ʙᴀɴɴᴇᴅ ᴍᴇᴍʙᴇʀ ʙᴇᴄᴀᴜsᴇ  ɴᴏᴛ  ᴊᴏɪɴᴇᴅ ғᴏʀᴄᴇ sᴜʙsʀɪʙᴇ ᴄʜᴀᴛ")
-                    await msg.delete()
-                    return
-                buttons = [[InlineKeyboardButton("✨ sᴜʙsᴄʀɪʙᴇ  ᴍʏ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]
-                if action == 'mute': 
-                    await msg.chat.restrict_member(user_id, ChatPermissions(can_send_messages=False))
-                    buttons.append([InlineKeyboardButton("ᴜɴᴍᴜᴛᴇ ᴍᴇ", callback_data=f"joined+{msg.from_user.id}")])
-                await msg.reply(
-                    f"ʜᴇʏ ᴅᴇᴀʀ  {msg.from_user.mention},\n\nʏᴏᴜ ᴍᴜsᴛ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ {mention} ᴛᴏ ᴄʜᴀᴛ ʜᴇʀᴇ ᴛʜᴇɴ ᴘʀᴇss ᴏɴ ᴜɴᴍᴜᴛᴇ ʙᴜᴛᴛᴏɴ.",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup(buttons),
-                    
-                )
-                await msg.delete()
-                await msg.stop_propagation()
+                    await msg.stop_propagation()
+
             except ChatWriteForbidden:
                 pass
+
     except ChatAdminRequired:
-        await msg.reply(f"ɪ ʜᴀᴠᴇ ʙᴇᴇɴ ᴅᴇᴍᴏᴛᴇᴅ ɪɴ `{force_chat}` (force subscribe chat)!")
+        await msg.reply(f"ɪ ʜᴀᴠᴇ ʙᴇᴇɴ ᴅᴇᴍᴏᴛᴇᴅ ɪɴ {force_chat} (force subscribe chat)")
+
+# Define the modified get_action function
+async def get_action(chat_id):
+    # Replace this with your logic to retrieve the desired action for the chat
+    # If no specific action is set, return 'warn' by default
+    action = await your_logic_to_retrieve_action(chat_id)
+    return action if action else 'warn'
